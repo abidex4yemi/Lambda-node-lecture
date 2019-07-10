52fbefa11f0f27ca28dc68045298b5f9a697d101
abidex4yemi <abidex4yemi@gmail.com>
2019-07-10 10:19:09 +0100

    Add helmet as dependencies

--- Day3/server.js
+++ Day3/server.js
@@ -1,17 +1,19 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+	const nameInsert = req.name ? ` ${req.name}` : '';
 
-  res.send(`
+	res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
