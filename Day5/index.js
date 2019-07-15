4e8a59e0c5cdcf588364c77915c85264c395e5a9
abidex4yemi <abidex4yemi@gmail.com>
2019-07-15 10:50:33 +0100

    Implement create user endpoint

--- Day5/index.js
+++ Day5/index.js
@@ -23,33 +23,57 @@ const getUserById = id => {
 		.where({ id });
 };
 
+const createUser = user => {
+	return db('users').insert(user);
+};
+
 app.get('/', (req, res) => {
 	return res.json('success!');
 });
 
-app.get('/users', async (req, res) => {
-	const users = await getAllUsers();
+app.get('/users', async (req, res, next) => {
+	try {
+		const users = await getAllUsers();
 
-	return res.status(200).json({
-		users
-	});
+		return res.status(200).json({
+			users
+		});
+	} catch (error) {
+		next(error);
+	}
 });
 
 app.get('/users/:id', async (req, res, next) => {
-	const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-	if (id) {
-		const user = await getUserById(id);
+		if (id) {
+			const user = await getUserById(id);
 
-		return res.status(200).json({
-			user
-		});
+			return res.status(200).json({
+				user
+			});
+		}
+
+		return next(new Error('User id is invalid'));
+	} catch (error) {
+		next(error);
 	}
-
-	return next(new Error('User id is invalid'));
 });
 
-app.use(function errorHandler(err, req, res, next) {
+app.post('/users', async (req, res, next) => {
+	try {
+		const newUserId = await createUser(req.body);
+
+		const newUser = await getUserById(newUserId[0]);
+
+		return res.status(201).json(newUser[0]);
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.use(function errorHandler(err, req, res) {
 	console.error('ERROR:', err);
 	res.status(500).json({
 		message: err.message,
