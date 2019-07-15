da4f7b628db386aa09cdfd0b67978e5ca7e072ca
abidex4yemi <abidex4yemi@gmail.com>
2019-07-15 10:14:53 +0100

    Create get all users endpoint and database configuration

--- Day5/index.js
+++ Day5/index.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const knex = require('knex');
 const app = express();
 
 app.use(express.json());
 
+const db = knex({
+	client: 'sqlite3',
+	connection: {
+		filename: './db/database.db3',
+		useNullAsDefault: false
+	}
+});
+
+const getAllUsers = db => {
+	return db('users');
+};
+
 app.get('/', (req, res, next) => {
-  res.json('success!');
+	res.json('success!');
+});
+
+app.get('/users', async (req, res) => {
+	const users = await getAllUsers(db);
+
+	return res.status(200).json({
+		users
+	});
 });
 
 app.use(function errorHandler(err, req, res, next) {
-  console.error('ERROR:', err);
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
+	console.error('ERROR:', err);
+	res.status(500).json({
+		message: err.message,
+		stack: err.stack
+	});
 });
 
 app.listen(4000, () => console.log('listening on 4000'));
