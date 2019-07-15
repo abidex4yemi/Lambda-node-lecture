845b3a345f1cad54da2626c7f81c7c212c8ee4e3
abidex4yemi <abidex4yemi@gmail.com>
2019-07-15 10:25:45 +0100

    Create get user by id endpoint

--- Day5/index.js
+++ Day5/index.js
@@ -8,24 +8,45 @@ const db = knex({
 	client: 'sqlite3',
 	connection: {
 		filename: './db/database.db3',
-		useNullAsDefault: false
+		useNullAsDefault: true
 	}
 });
 
-const getAllUsers = db => {
-	return db('users');
+const getAllUsers = () => {
+	return db.select().from('users');
 };
 
-app.get('/', (req, res, next) => {
-	res.json('success!');
+const getUserById = id => {
+	return db
+		.select()
+		.from('users')
+		.where({ id });
+};
+
+app.get('/', (req, res) => {
+	return res.json('success!');
 });
 
 app.get('/users', async (req, res) => {
-	const users = await getAllUsers(db);
+	const users = await getAllUsers();
 
 	return res.status(200).json({
 		users
 	});
+});
+
+app.get('/users/:id', async (req, res, next) => {
+	const { id } = req.params;
+
+	if (id) {
+		const user = await getUserById(id);
+
+		return res.status(200).json({
+			user
+		});
+	}
+
+	return next(new Error('User id is invalid'));
 });
 
 app.use(function errorHandler(err, req, res, next) {
