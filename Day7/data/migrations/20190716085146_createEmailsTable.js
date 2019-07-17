7e1a32681fc4b004eadb7a50734a50986242b63d
abidex4yemi <abidex4yemi@gmail.com>
2019-07-17 10:15:54 +0100

    fix: modify emails table to reference users table

--- Day7/data/migrations/20190716085146_createEmailsTable.js
+++ Day7/data/migrations/20190716085146_createEmailsTable.js
@@ -2,7 +2,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('emails', table => {
     table.increments();
     table.text('email', 128).unique().notNullable();
-    table.integer('user_id');
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('users.id');
   });
 };
 
--- Day7/data/seeds/02-emails.js
+++ Day7/data/seeds/02-emails.js
@@ -1,13 +1,14 @@
-exports.seed = function (knex) {
-  return knex('emails').truncate()
-    .then(function () {
-      return knex('emails').insert([
-        { email: 'apple@apple.com', user_id: 1 },
-        { email: 'orange@orange.com', user_id: 2 },
-        { email: 'pear@pear.com', user_id: 3 },
-        { email: 'carrot@carrot.com', user_id: 3 },
-        { email: 'tomato@tomato.com' },
-        { email: 'garlic@garlic.com' },
-      ]);
-    });
+exports.seed = function(knex) {
+	return knex('emails')
+		.truncate()
+		.then(function() {
+			return knex('emails').insert([
+				{ email: 'apple@apple.com', user_id: 1 },
+				{ email: 'orange@orange.com', user_id: 2 },
+				{ email: 'pear@pear.com', user_id: 3 },
+				{ email: 'carrot@carrot.com', user_id: 3 },
+				{ email: 'tomato@tomato.com', user_id: 2 },
+				{ email: 'garlic@garlic.com', user_id: 3 }
+			]);
+		});
 };
