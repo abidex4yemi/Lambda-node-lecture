f0b51e5ef059c969f4e8f7d1dd2abfd586606902
abidex4yemi <abidex4yemi@gmail.com>
2019-07-18 10:37:53 +0100

    Create enrollments schema

--- Day8/data/migrations/20190717114158_createEnrolmentsTb.js
+++ Day8/data/migrations/20190717114158_createEnrolmentsTb.js
@@ -1,8 +1,19 @@
-
-exports.up = function (knex) {
-
+exports.up = function(knex) {
+	return knex.schema.createTable('enrollments', table => {
+		table.increments();
+		table.integer('user_id');
+		table.integer('course_id');
+		table
+			.foreign('user_id')
+			.references('id')
+			.inTable('users');
+		table
+			.foreign('course_id')
+			.references('id')
+			.inTable('courses');
+	});
 };
 
-exports.down = function (knex) {
-
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('enrollments');
 };
