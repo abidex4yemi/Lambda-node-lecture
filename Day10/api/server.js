60711dca936833b7e2e1aeb64a3d0ecaad60a7d4
abidex4yemi <abidex4yemi@gmail.com>
2019-07-23 11:19:04 +0100

    Install knex session store

--- Day10/api/server.js
+++ Day10/api/server.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const knex = require('../database/dbConfig');
+const knexSessionStore = require('connect-session-knex')(session);
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
+const store = new knexSessionStore({ knex });
+
 const server = express();
+
+server.use(
+	session({
+		store,
+		name: 'whatwig',
+		secret: 'Keep it secret, simple',
+		cookie: {
+			maxAge: 1000 * 60 * 60,
+			secure: false,
+			httpOnly: true
+		},
+		resave: false,
+		saveUninitialized: true
+	})
+);
 
 server.use(helmet());
 server.use(express.json());
-server.use(cookieParser());
+// server.use(cookieParser());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+	res.json({ api: 'up' });
 });
 
 module.exports = server;
