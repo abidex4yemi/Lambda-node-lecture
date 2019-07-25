a281b55f2ed55aed793f1ac19c791f792ab15b16
abidex4yemi <abidex4yemi@gmail.com>
2019-07-25 09:47:29 +0100

    Test sum function

--- Day12/api/server.spec.js
+++ Day12/api/server.spec.js
@@ -1,3 +1,12 @@
-it('works!!!', () => {
-  
+function sum(a, b) {
+  return a + b;
+}
+
+describe('sum() function', () => {
+  it('Should sum two numbers', () => {
+    const actualOutput = sum(1, 2);
+    expect(actualOutput).toBe(3);
+  });
+
+  it('Should return null if passed an arg which is not number', () => {});
 });
